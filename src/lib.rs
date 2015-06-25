2afb583f3233c1ab900b1b341c1b1a67e716b22b
Sven Nilsen <bvssvni@gmail.com>
2015-06-25 21:59:14 +0200

    Started on meta syntax
    Closes https://github.com/PistonDevelopers/opengex/issues/12

--- src/lib.rs
+++ src/lib.rs
@@ -4,59 +4,42 @@
 
 extern crate piston_meta;
 
-use piston_meta::*;
-use std::rc::Rc;
+#[test]
+fn test_syntax() {
+    use piston_meta::*;
+    use std::fs::File;
+    use std::io::Read;
+    use std::path::PathBuf;
 
-pub const DEBUG_ID_SIZE: usize = 1000;
+    let rules = bootstrap::rules();
+    let opengex_syntax: PathBuf = "assets/opengex-syntax.txt".into();
+    let mut file_h = File::open(opengex_syntax).unwrap();
+    let mut source = String::new();
+    file_h.read_to_string(&mut source).unwrap();
+    let data = parse(&rules, &source);
+    let data = match data {
+        Ok(data) => data,
+        Err((range, err)) => {
+            // Report the error to standard error output.
+            ParseStdErr::new(&source).error(range, err);
+            assert!(false);
+            unreachable!();
+        }
+    };
 
-/// Gets the rules for parsing OpenGEX.
-pub fn rules(debug_id_offset: usize) -> Rule {
-    fn name(text: &str) -> Rc<String> { Rc::new(text.into()) }
-
-    /*
-    let animation_name = name("Animation");
-    let atten_name = name("Atten");
-    let bone_count_array_name = name("BoneCountArray");
-    let bone_index_array_name = name("BoneIndexArray");
-    let bone_node_name = name("BoneNode");
-    let bone_ref_array_name = name("BoneRefArray");
-    let bone_weight_array_name = name("BoneWeightArray");
-    let camera_node_name = name("CameraNode");
-    let camera_object_name = name("CameraObject");
-    let clip_name = name("Clip");
-    let color_name = name("Color");
-    let extension_name = name("Extension");
-    let geometry_node_name = name("GeometryNode");
-    let geometry_object_name = name("GeometryObject");
-    let index_array_name = name("IndexArray");
-    let key_name = name("Key");
-    let light_node_name = name("LightNode");
-    let light_object_name = name("LightObject");
-    let material_name = name("Material");
-    let material_ref_name = name("MaterialRef");
-    let mesh_name = name("Mesh");
-    let metric_name = name("Metric");
-    let morph_name = name("Morph");
-    let morph_weight_name = name("MorphWeight");
-    let name_name = name("Name");
-    let node_name = name("Node");
-    let object_ref_name = name("ObjectRef");
-    let param_name = name("Param");
-    let rotation_name = name("Rotation");
-    let scale_name = name("Scale");
-    let skeleton_name = name("Skeleton");
-    let skin_name = name("Skin");
-    let texture_name = name("Texture");
-    let time_name = name("Time");
-    let track_name = name("Track");
-    let transform_name = name("Transform");
-    let translation_name = name("Translation");
-    let value_name = name("Value");
-    let vertex_array_name = name("VertexArray");
-    */
-
-    Rule::Whitespace(Whitespace {
-        debug_id: 0,
-        optional: true
-    })
+    let rules = bootstrap::convert(&data, &mut vec![]).unwrap();
+    let opengex_syntax: PathBuf = "assets/cube.ogex".into();
+    let mut file_h = File::open(opengex_syntax).unwrap();
+    let mut source = String::new();
+    file_h.read_to_string(&mut source).unwrap();
+    let data = parse(&rules, &source);
+    let data = match data {
+        Ok(data) => data,
+        Err((range, err)) => {
+            // Report the error to standard error output.
+            ParseStdErr::new(&source).error(range, err);
+            assert!(false);
+            unreachable!();
+        }
+    };
 }

668cdc269e14a87f4c12cf5ddb1901fb034742d0
Sven Nilsen <bvssvni@gmail.com>
2015-07-12 00:49:56 +0200

    Fixed time metric rule

--- src/lib.rs
+++ src/lib.rs
@@ -33,7 +33,7 @@ fn test_syntax() {
     let mut source = String::new();
     file_h.read_to_string(&mut source).unwrap();
     let data = parse(&rules, &source);
-    let _data = match data {
+    let data = match data {
         Ok(data) => data,
         Err((range, err)) => {
             // Report the error to standard error output.
@@ -42,4 +42,6 @@ fn test_syntax() {
             unreachable!();
         }
     };
+    json::print(&data);
+    // assert!(false);
 }

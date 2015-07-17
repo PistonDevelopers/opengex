eb5a3a2cdfa1ce25c5bd2c47c9a6a444a3ad02aa
Sven Nilsen <bvssvni@gmail.com>
2015-07-17 20:04:03 +0200

    Updated to latest piston_meta

--- src/lib.rs
+++ src/lib.rs
@@ -9,39 +9,17 @@ fn test_syntax() {
     use piston_meta::*;
     use std::fs::File;
     use std::io::Read;
-    use std::path::PathBuf;
 
-    let rules = bootstrap::rules();
-    let opengex_syntax: PathBuf = "assets/opengex-syntax.txt".into();
-    let mut file_h = File::open(opengex_syntax).unwrap();
+    let mut file_h = File::open("assets/opengex-syntax.txt").unwrap();
     let mut source = String::new();
     file_h.read_to_string(&mut source).unwrap();
-    let data = parse(&rules, &source);
-    let data = match data {
-        Ok(data) => data,
-        Err((range, err)) => {
-            // Report the error to standard error output.
-            ParseStdErr::new(&source).error(range, err);
-            assert!(false);
-            unreachable!();
-        }
-    };
-
-    let rules = bootstrap::convert(&data, &mut vec![]).unwrap();
-    let opengex_syntax: PathBuf = "assets/cube.ogex".into();
-    let mut file_h = File::open(opengex_syntax).unwrap();
+    let rules = stderr_unwrap(&source, syntax(&source));
+    
+    let mut file_h = File::open("assets/cube.ogex").unwrap();
     let mut source = String::new();
     file_h.read_to_string(&mut source).unwrap();
-    let data = parse(&rules, &source);
-    let data = match data {
-        Ok(data) => data,
-        Err((range, err)) => {
-            // Report the error to standard error output.
-            ParseStdErr::new(&source).error(range, err);
-            assert!(false);
-            unreachable!();
-        }
-    };
+    
+    let data = stderr_unwrap(&source, parse(&rules, &source));
     json::print(&data);
     // assert!(false);
 }

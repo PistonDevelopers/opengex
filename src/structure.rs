5fcd1ffa783e3933c65a1d4e49d367117e6be75b
Jeroen Bollen <jbinero@gmail.com>
2015-10-11 23:17:34 +0200

    Implemented LightObject, and switched from Rc to Arc.

--- src/structure.rs
+++ src/structure.rs
@@ -5,7 +5,7 @@
 //! documentation, please go to http://opengex.org.
 
 use std::collections::HashMap;
-use std::rc::Rc;
+use std::sync::Arc;
 
 /// The Material structure contains information about a material. Material structures are
 /// referenced by geometry nodes through MaterialRef structures belonging to GeometryNode
@@ -31,9 +31,12 @@ pub enum Color {
     Rgba(f32, f32, f32, f32)
 }
 
-/// This is a map of different Param structures, for convenience.
-pub type ParamMap = HashMap<String, f32>;
+/// The Param structure contains just a float. While the OpenGEX specification defines a Param as a
+/// key-value pair, this is better implemented through a HashMap. See ParamMap.
+pub type Param = f32;
 
+/// This is a map of different Param structures, for convenience.
+pub type ParamMap = HashMap<String, Param>;
 /// The Name structure holds the name of a node, morph target, material, or animation clip.
 pub type Name = String;
 
@@ -165,9 +168,9 @@ pub struct Track {
 /// Enum wrapping over all possible animation track targets.
 pub enum TrackTarget {
     /// A Transformation structure. See enum to see possibilities.
-    Transformation(Rc<Transformation>),
+    Transformation(Arc<Transformation>),
     /// A MorphWeight structure.
-    MorphWeight(Rc<MorphWeight>)
+    MorphWeight(Arc<MorphWeight>)
 }
 
 /// The Time structure contains key time data in an animation track.
@@ -203,7 +206,7 @@ pub enum Value {
     /// The values are interpolated on a tension-continuity-bias (TCB) spline.
     ///
     /// The values in each tuple in the vector are the "value", "tension", "bias" and "continuity"
-    /// values of the TCB spline respecitvely. The data contained in these last three values is
+    /// values of the TCB spline respecitvely. The data contained in these last three values are
     /// always scalar.
     Tcb(Vec<(f32, f32, f32, f32)>)
 }
@@ -221,4 +224,102 @@ pub struct MorphWeight {
     pub target_index: u32,
     /// The weight this MorphWeight structure represents.
     pub weight: f32,
+}
+
+/// The LightObject struture contains data for a light object. Multiple LightNode structures may
+/// reference a single LightObject. This allows a scene to contain multiple instances of the same
+/// light, with different transformations.
+pub struct LightObject {
+    /// The type of light emitted by this LightObject.
+    pub light_type: LightType,
+    /// Whether this LightObject casts shadows. This can be overiden by the LightNode referencing
+    /// this LightObject.
+    pub cast_shadow: bool,
+    /// The colors associated with this LightObject.
+    ///
+    /// The OpenGEX specification only references one type of color: "light". This is the main
+    /// color of light emitted by the light souArce. This defaults to an RGB value of
+    /// (1.0, 1.0, 1.0).
+    ///
+    /// There can be any other number of colors in this HashMap, for application-specific kinds.
+    pub colors: HashMap<String, Color>,
+    /// The parameters associated with this LightObject.
+    ///
+    /// The OpenGEX specification only references one type of parameter: "intensity". This is the
+    /// intensity of the light emitted by the light souArce. This defaults to 0.
+    ///
+    /// There can be any other number of parameters in this HashMap, for application-specific
+    /// kinds.
+    pub params: ParamMap,
+    /// The textures associated with this LightObject.
+    ///
+    /// The OpenGEX specification only references one type of texture: "projection". This is an
+    /// optional texture projection of this LightObject. The texture map should be oriented so that
+    /// the right direction is aligned to the object-space positive x-axis, and the up direction is
+    /// aligned to the object-space positive y-axis.
+    ///
+    /// There can be any other number of textures in this HashMap, for application-specific kinds.
+    pub textures: HashMap<String, Texture>,
+    /// Any number of attenuation functions to be applied to the LightObject. The values produced
+    /// by all of them are multiplied together to determine the intensity of the light reaching
+    /// any particular point in space.
+    pub attenuations: Atten
+}
+
+/// This is an helper-enum representing all different types of lights that a LightObject can emit.
+pub enum LightType {
+    /// The light souArce is to be treated as if it were infinitely far away so its rays are
+    /// parallel. In object space, the rays point in the direction of the negative z-axis.
+    Infinite,
+    /// The lght souArce is a point light that radiates in all directions.
+    Point,
+    /// The light source is a spot light that radiates from a single points byt in a limited range
+    /// of directions In object space, the primary direction is the negative z-axis.
+    Spot
+}
+
+/// The Atten structure specifies an attenuation function for a light object.
+pub struct Atten {
+    /// The kind of attenuation.
+    pub kind: AttenuationKind,
+    /// The type of curve defining the attenuation.
+    pub curve: AttenuationCurve,
+    /// Any parameters associated with this Atten structure.
+    ///
+    /// For the meaning of the parameters, please refer to the official OpenGEX documentation.
+    /// There can exist application-defined parameters.
+    pub params: ParamMap
+}
+
+/// A helper enum representing different kinds of attenuation functions.
+pub enum AttenuationKind {
+    /// The input to the attenuation function is the radial distance from the LightObject the
+    /// parent Atten structure is associated with.
+    Distance,
+    /// The input to the attenuation function is the angle formed between the negative z-axis and
+    /// the direction to the point being illuminated in object space.
+    ///
+    /// The result of this function should be raised to the power value of a potential "power"
+    /// paramter present in the parent Atten structure.
+    Angle,
+    /// The input ot the attenuation function is the cosine of the angle formed between the
+    /// negative z-axis and the direction to the point being illuminated in objet space.
+    ///
+    /// The result of this function should be raised to the power value of a potential "power"
+    /// paramter present in the parent Atten structure.
+    CosAngle
+}
+
+/// A helper enum representing different kinds of curves for an attenuation function.
+///
+/// For exact formulas, please refer to the offical OpenGEX documentation.
+pub enum AttenuationCurve {
+    /// The attenuation is a linear function.
+    Linear,
+    /// The attenuation is given by a cubic-smooth-step function.
+    Cubic,
+    /// The attenuation is given by the inverse function.
+    Inverse,
+    /// The attenuation is given by the inverse square function.
+    InverseSquare
 }

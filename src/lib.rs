50cdb46c7e9d4882615e89635352c37627619c3f
Jeroen Bollen <jbinero@gmail.com>
2015-10-14 16:48:09 +0200

    Partially implemented GeometryObject, and switched out some HashMaps for VecMaps for better? performance.

--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,7 @@
 #![deny(missing_docs)]
 #![feature(non_ascii_idents)]
 
+/// A library containing `VecMap`, a `HashMap`-like structure optimized for small integer keys.
+extern crate vec_map;
+
 pub mod structure;

9b40bc6fea375077aaa249de53f15b378978f2df
Matthew Johnston <warmwaffles@gmail.com>
2016-01-08 10:56:54 -0600

    Changed bezier non ascii name
    This is a breaking change for any libs that depend on this library.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 //! Library for parsing OpenGEX formatted files.
 
 #![deny(missing_docs)]
-#![feature(non_ascii_idents)]
 
 /// A library containing `VecMap`, a `HashMap`-like structure optimized for small integer keys.
 extern crate vec_map;
